ee5543fd998a30d2c62d12be6ca03dfc5c35ca13
张玉峰 <3252667584@qq.com>
2026-09-22 11:41:14 +0800

    Updates

--- assets/useDragConnector-ev6ER_P-.js
+++
@@ -1,1 +0,0 @@
-import{an as e,sn as t,yn as n}from"./index-CPzMchj1.js";import{d as r,l as i,p as a}from"./constants-OP6rBRIc.js";function o(o,s,c={}){let{isPreview:l,guard:u}=i(s),d=null,f=u(()=>{!o.value||d||(d=r.connectDraggable(o.value,s))});function p(){d?.(),d=null}t(()=>{o.value&&(a.register(s,o.value,c.isCanvas??!1),f())}),n(l,e=>{e?p():f()}),e(()=>{p(),a.unregister(s)})}export{o as t};

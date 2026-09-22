1255e58758b928fa942e015f14c695eb13fdf248
张玉峰 <3252667584@qq.com>
2026-09-22 13:03:39 +0800

    Updates

--- assets/useDragConnector-C8GQiI4X.js
+++
@@ -1,1 +0,0 @@
-import{bn as e,cn as t,on as n}from"./index-CKmrH_IB.js";import{f as r,m as i,u as a}from"./constants-BErGJ17q.js";function o(o,s,c={}){let{isPreview:l,guard:u}=a(s),d=null,f=u(()=>{!o.value||d||(d=r.connectDraggable(o.value,s))});function p(){d?.(),d=null}t(()=>{o.value&&(i.register(s,o.value,c.isCanvas??!1),f())}),e(l,e=>{e?p():f()}),n(()=>{p(),i.unregister(s)})}export{o as t};
